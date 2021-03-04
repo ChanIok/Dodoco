00dd78df8d7f0b6f77376961493f759d290309b7
ChanIok <654232825@qq.com>
2021-03-04 09:49:33 +0800

    测试

--- test.js
+++ test.js
@@ -2,20 +2,35 @@ let jsdelivrUrl = 'https://cdn.jsdelivr.net/gh/ChanIok/Dodoco@main/updateResourc
 
 const axios = require('axios')
 
+
+const https = require("https")
+https.get(jsdelivrUrl + 'latest.json' + '?t=' + Date.parse(new Date()) / 1000, (res) => {
+
+
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
+
+}).on('error', (e) => {
+    console.error(e);
+});
 // 获取最新版本信息
 
-console.log("start-to-getVersion")
-axios({
-    url: jsdelivrUrl + 'latest.json' + "?t=" + Date.parse(new Date()) / 1000,
-    // url: 'http://127.0.0.1:10996/latest.json',
-    method: 'get',
-    timeout: 20000,
-}).then(function (latestResData) {
+// console.log("start-to-getVersion")
+// axios({
+//     url: jsdelivrUrl + 'latest.json',
+//     // url: 'http://127.0.0.1:10996/latest.json',
+//     method: 'get',
+//     params: {
+//         t: Date.parse(new Date()) / 1000
+//     },
+//     timeout: 20000,
+// }).then(function (latestResData) {
 
 
-    latestRes = latestResData.data
-    console.log(latestRes)
-}, function (err) {
+//     latestRes = latestResData.data
+//     console.log(latestRes)
+// }, function (err) {
 
-    console.log("get-latest-err")
-})
+//     console.log("get-latest-err")
+// })
