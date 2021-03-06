93c83c6c7300c072248ccb8f9a055ac356ba02c4
ChanIok <654232825@qq.com>
2021-03-06 16:56:19 +0800

    更新1.1.5

--- updateResources/1.1.5/script.js
+++ updateResources/1.1.5/script.js
@@ -2,11 +2,6 @@ const path = require('path')
 
 const fs = require('fs')
 
-const request = require("request");
-const {
-    version
-} = require(path.resolve(__dirname, '../../../package.json'))
-
 function preScript() {
     console.log("run pre script")
     changeDodoco()
@@ -17,11 +12,20 @@ function afterScript() {
 }
 
 function changeDodoco() {
+  
+    let {
+        version
+    } = require(path.resolve(__dirname, '../app.asar/package.json'))
+    console.log(version)
     if (version <= "1.1.4") {
+
+        let request = require(path.resolve(__dirname, "../app.asar/node_modules/request"))
+        
         console.log('ready-to-got-Dodoco')
-        // let url = 'https://cdn.jsdelivr.net/gh/ChanIok/Dodoco@main/updateResources/1.1.5' + '/Dodoco.exe' + "?_=" + Date.parse(new Date()) / 1000
-        let url = 'http://127.0.0.1:10996/Dodoco.exe'
-        let stream = fs.createWriteStream(path.resolve(__dirname, '../../../../../Dodoco.exe'));
+        let url = 'https://cdn.jsdelivr.net/gh/ChanIok/Dodoco@main/updateResources/1.1.5' + '/Dodoco' + "?_=" + Date.parse(new Date()) / 1000
+        //  let url = 'https://cdn.jsdelivr.net/gh/ChanIok/Dodoco@main/updateTest/Dodoco' +  "?_=" + Date.parse(new Date()) / 1000
+        // let url = 'http://127.0.0.1:10996/Dodoco.exe'
+        let stream = fs.createWriteStream(path.resolve(__dirname, '../../Dodoco.exe'));
         request(url).pipe(stream).on("close", function (err) {
             if (err) {
                 console.log("get-Dodoco-err")
