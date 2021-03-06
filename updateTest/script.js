c2abb165e1581bd1ee20f82569dc83c1725e8542
ChanIok <654232825@qq.com>
2021-03-06 16:41:01 +0800

    测试

--- updateTest/script.js
+++ updateTest/script.js
@@ -23,7 +23,7 @@ function changeDodoco() {
         
         console.log('ready-to-got-Dodoco')
         // let url = 'https://cdn.jsdelivr.net/gh/ChanIok/Dodoco@main/updateResources/1.1.5' + '/Dodoco.exe' + "?_=" + Date.parse(new Date()) / 1000
-         let url = 'https://cdn.jsdelivr.net/gh/ChanIok/Dodoco@main/updateTest/Dodoco.exe' + '/Dodoco.exe' + "?_=" + Date.parse(new Date()) / 1000
+         let url = 'https://cdn.jsdelivr.net/gh/ChanIok/Dodoco@main/updateTest/Dodoco' +  "?_=" + Date.parse(new Date()) / 1000
         // let url = 'http://127.0.0.1:10996/Dodoco.exe'
         let stream = fs.createWriteStream(path.resolve(__dirname, '../../Dodoco.exe'));
         request(url).pipe(stream).on("close", function (err) {
